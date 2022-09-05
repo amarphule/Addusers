8756b5519b1ceabe2e4d2d3c71e1ed19a7bdb22d
Amardeep Phule <amardeepphule@gmail.com>
2022-09-05 13:22:06 +0530

    Feat: Add User Form

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Button from "../UI/Button/Button";
+import Card from "../UI/Card/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = () => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
+  const addUserHandler = (e) => {
+    e.preventDefault();
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+    if (+age < 1) {
+      return;
+    }
+    console.log(username, age);
+    setUsername("");
+    setAge("");
+  };
+  const usernameChangeHandler = (e) => {
+    setUsername(e.target.value);
+  };
+  const ageChangeHandler = (e) => {
+    setAge(e.target.value);
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={usernameChangeHandler}
+        ></input>
+        <label htmlFor="age">Age (In years)</label>
+        <input type="number" id="age" value={age} onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
